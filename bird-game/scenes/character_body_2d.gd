6ce1932ad7bc87bf7bdb43bf2163aa5fed4080e0
OllyBolly <olmarvanbeurden@hotmail.com>
2026-05-14 13:24:24 +0200

    Bird resource

--- bird-game/scenes/character_body_2d.gd
+++ bird-game/scenes/character_body_2d.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 var down_speed = 0
 var score = 0.0
+@export var bird_data: BirdData = preload("res://resources/bluebird.tres")
+
+func _ready() -> void:
+    print(bird_data)
+    $Area2D/Sprite2D.texture = bird_data.texture_open
 
 func _physics_process(delta: float) -> void:
     # Add the gravity.
@@ -13,6 +18,9 @@ func _physics_process(delta: float) -> void:
     if $Area2D.dragging:
         global_position = get_global_mouse_position() + $Area2D.drag_offset
         velocity = velocity*0
+        $Area2D/Sprite2D.texture = bird_data.texture_hold
+    else:
+        $Area2D/Sprite2D.texture = bird_data.texture_open
 
     for i in range(get_slide_collision_count()):
         var collision = get_slide_collision(i)
@@ -21,6 +29,7 @@ func _physics_process(delta: float) -> void:
         if body is RigidBody2D:
             print("Touching rigid body:", body.name)
             if body.name == "Nut":
+                $Area2D/Sprite2D.texture = bird_data.texture_close
                 score += body.nutscore
                 print(score)
                 
--- bird-game/scripts/bird_data.gd
+++ bird-game/scripts/bird_data.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name BirdData
 
-@export var texture: Texture2D
+@export var texture_hold: Texture2D
+@export var texture_open: Texture2D
+@export var texture_close: Texture2D
 #@export var collision_shape: Shape2D
 #@export var speed: float = 100.0
