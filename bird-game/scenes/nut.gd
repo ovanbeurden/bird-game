b45d5f25ca255387f53fbe1a27416eacb6ad6567
Siert <siert.sebus@capgemini.com>
2026-05-14 14:41:20 +0200

    Update nut code. Add empty nutspawner files.

--- bird-game/scenes/nut.gd
+++ bird-game/scenes/nut.gd
@@ -1,14 +1,16 @@
-extends RigidBody2D
+extends Area2D
 
 var nutscore = 20
+@export var speed = 100.0
 @export var nut_data: NutData
 @onready var sprite = $Walnut
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    sprite.texture = nut_data.texture
+	sprite.texture = nut_data.texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    sprite.rotate(delta)
+	sprite.rotate(delta)
+	position.y += speed * delta
