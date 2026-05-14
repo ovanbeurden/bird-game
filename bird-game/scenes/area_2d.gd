dd74a00ef207b5fe55d2f3f422d6a802aa6512a7
OllyBolly <olmarvanbeurden@hotmail.com>
2026-05-14 14:26:15 +0200

    Een vogelbeest

--- bird-game/scenes/area_2d.gd
+++ bird-game/scenes/area_2d.gd
@@ -3,20 +3,21 @@ extends Area2D
 var dragging = false
 var hovering := false
 var drag_offset = Vector2.ZERO
+@onready var drag_man = get_tree().get_root().get_node("BirdGame/DragMan")
 
 func _ready() -> void:
     mouse_entered.connect(func(): hovering = true)
     mouse_exited.connect(func(): hovering = false)
 
 func _input(event):
+    
     if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-        if event.pressed and hovering:
+        if event.pressed and hovering and drag_man.current_dragged_object == null:
             dragging = true
-        else:
+            drag_man.current_dragged_object = self
+        elif drag_man.current_dragged_object == self:
             dragging = false
-            #
-#func update_sprite():
-    #$Sprite2D.texture = 
+            drag_man.current_dragged_object = null
 
 func _process(delta):
     pass
--- bird-game/scenes/node.gd
+++ bird-game/scenes/node.gd
@@ -1,9 +1,12 @@
-extends Node2D
+extends Node
+
+var current_dragged_object = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     pass # Replace with function body.
-    
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
