d28174dd1306029fdca61eb314c59a4dc9198419
Siert <siert.sebus@capgemini.com>
2026-05-14 17:35:41 +0200

    Slightly more funny font

--- bird-game/scenes/camera.gd
+++ bird-game/scenes/camera.gd
@@ -11,40 +11,40 @@ var global_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    hide_system_cursor = not OS.has_feature("editor")
+	hide_system_cursor = not OS.has_feature("editor")
 
-    var cursor_open_img = load("res://sprites/cursor open.png").get_image()
-    cursor_open_img.resize(mouse_size, mouse_size)
-    cursor_open = ImageTexture.create_from_image(cursor_open_img)    
-    var cursor_close_img = load("res://sprites/cursor close.png").get_image()
-    cursor_close_img.resize(mouse_size, mouse_size)
-    cursor_close = ImageTexture.create_from_image(cursor_close_img)
-    cursor_sprite.texture = cursor_open
-    Input.mouse_mode = Input.MOUSE_MODE_HIDDEN if hide_system_cursor else Input.MOUSE_MODE_VISIBLE
-    $GameOver.visible = false
-    
+	var cursor_open_img = load("res://sprites/cursor open.png").get_image()
+	cursor_open_img.resize(mouse_size, mouse_size)
+	cursor_open = ImageTexture.create_from_image(cursor_open_img)    
+	var cursor_close_img = load("res://sprites/cursor close.png").get_image()
+	cursor_close_img.resize(mouse_size, mouse_size)
+	cursor_close = ImageTexture.create_from_image(cursor_close_img)
+	cursor_sprite.texture = cursor_open
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN if hide_system_cursor else Input.MOUSE_MODE_VISIBLE
+	$GameOver.visible = false
+	
 
 func _process(_delta: float) -> void:
-    cursor_sprite.position = get_viewport().get_mouse_position()
-    $Label.text = "Score: " + str(global_score)
-    birdlife = $BirdGuy.birdstatus + $BirdGuy2.birdstatus + $BirdGuy3.birdstatus
-    if birdlife == 0:
-        game_over()
-        # TODO: stop everything and game over
-        
+	cursor_sprite.position = get_viewport().get_mouse_position()
+	$Label.text = "Score: " + str(global_score)
+	birdlife = $BirdGuy.birdstatus + $BirdGuy2.birdstatus + $BirdGuy3.birdstatus
+	if birdlife == 0:
+		game_over()
+		# TODO: stop everything and game over
+		
 
 
 func _input(event):
-    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-        if event.pressed:
-            cursor_sprite.texture = cursor_close
-        else:
-            cursor_sprite.texture = cursor_open
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			cursor_sprite.texture = cursor_close
+		else:
+			cursor_sprite.texture = cursor_open
 
 func _exit_tree() -> void:
-    Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-            
+			
 func game_over():
-    get_tree().paused = true
-    $GameOver.visible = true
+	get_tree().paused = true
+	$GameOver.visible = true
