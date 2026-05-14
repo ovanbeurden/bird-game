9800b15e2e87e8b6d1ad6254cd416b711ade66ce
Siert <siert.sebus@capgemini.com>
2026-05-14 15:06:32 +0200

    Fix bird eat animation.

--- bird-game/scenes/character_body_2d.gd
+++ bird-game/scenes/character_body_2d.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var down_speed = 0
 var score = 0.0
+var mouth_close_time_left := 0.0
 @export var bird_data: BirdData = preload("res://resources/bluebird.tres")
 
 func _ready() -> void:
@@ -9,6 +10,8 @@ func _ready() -> void:
     $Area2D/Sprite2D.texture = bird_data.texture_open
 
 func _physics_process(delta: float) -> void:
+    mouth_close_time_left = maxf(0.0, mouth_close_time_left - delta)
+
     # Add the gravity.
     if not is_on_floor():
         velocity += get_gravity() * delta
@@ -18,6 +21,10 @@ func _physics_process(delta: float) -> void:
     if $Area2D.dragging:
         global_position = get_global_mouse_position() + $Area2D.drag_offset
         velocity = velocity*0
+
+    if mouth_close_time_left > 0.0:
+        $Area2D/Sprite2D.texture = bird_data.texture_close
+    elif $Area2D.dragging:
         $Area2D/Sprite2D.texture = bird_data.texture_hold
     else:
         $Area2D/Sprite2D.texture = bird_data.texture_open
@@ -35,6 +42,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
     if bird_data.color.to_lower() != area.nut_data.color.to_lower():
         return
 
+    mouth_close_time_left = 0.5
     $Area2D/Sprite2D.texture = bird_data.texture_close
     score += area.nutscore
     area.queue_free()
