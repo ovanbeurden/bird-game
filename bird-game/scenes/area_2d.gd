cb0bed4f481ca939c74a628caf64294398f5f1b3
OllyBolly <olmarvanbeurden@hotmail.com>
2026-05-14 12:32:04 +0200

    No snappies

--- bird-game/scenes/area_2d.gd
+++ bird-game/scenes/area_2d.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 var dragging = false
+var hovering := false
 var drag_offset = Vector2.ZERO
+
+func _ready() -> void:
+    mouse_entered.connect(func(): hovering = true)
+    mouse_exited.connect(func(): hovering = false)
 
 func _input(event):
     if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-
-        if event.pressed:
+        if event.pressed and hovering:
             dragging = true
         else:
             dragging = false
