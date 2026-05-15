544ff377b3299806627dd7909e036cbd1ba53c86
Siert <siert.sebus@capgemini.com>
2026-05-15 12:56:53 +0200

    Small fixes. Do the export.

--- bird-game/scenes/bomb.gd
+++ bird-game/scenes/bomb.gd
@@ -6,10 +6,6 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     $AnimatedSprite2D.play("default")
-
-
-# Called every frame. 'delta' is the elapsed t$AnimatedSprite2D.play("idle")since the previous frame.
-func _process(delta: float) -> void:
     area_entered.connect(_on_area_2d_area_entered)
 
 
